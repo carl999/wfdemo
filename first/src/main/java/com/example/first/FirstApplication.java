7b69206377476399d4f2a494b3df04baeaea968f
CPUheart <867949220@qq.com>
2019-07-10 15:41:52 +0800

    Westley_demo

---
+++ first/src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,20 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstApplication {
+    @RequestMapping("/")
+    String index() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+
+}
